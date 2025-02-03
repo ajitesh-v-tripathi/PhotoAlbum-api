05d9577d208955147fef6507bd965f4f88edca09
ajitesh-v-tripathi <ajitesh.tripathi@vivaops.ai>
2025-02-04 01:08:03 +0530

    Update securityScan.groovy

--- vars/securityScan.groovy
+++ vars/securityScan.groovy
@@ -1,6 +1,6 @@
 def call(String dockerImage, String dockerTag) {
     echo "Starting security scan for image: ${dockerImage}:${dockerTag}"
 
-    echo "Scanning successful and uploading report to the artifacts:
+    echo "Scanning successful and uploading report to the artifacts"
     // Or use another security scanning tool, depending on your requirements.
 }
